8a212b1feb6010f6fac2f5bd34bdbb04e606cfbb
Angela Fuller <angela@Angelas-MacBook-Air.local>
2023-08-14 21:29:01 -0400

    Add comments

--- main.py
+++ main.py
@@ -7,19 +7,22 @@ import uvicorn
 
 app = FastAPI()
 
-
+# Handle GroupMe messages, return 200 status(success)
 @app.post('/', status_code=200)
 def handle_groupme_message(text: str = Body(), sender_type: str = Body()):
     if 'cat' in text.lower() and sender_type == 'user':
         post_groupme_message(get_cat_pic())
-        return 
+        return
 
 def post_groupme_message(link):
+    # GroupMe's Bot API Endpoint
     url = 'https://api.groupme.com/v3/bots/post'
+    # Bot message data
     groupme_parameters = {
         'bot_id': os.getenv('BOT_ID'), 
         'text': link
     }
+    # Post message
     requests.post(url, json = groupme_parameters)
 
 # Check app liveness
@@ -27,7 +30,7 @@ def post_groupme_message(link):
 def run_live():
     return "live"
 
-# Used by groupme url validation
+# Used by GroupMe url validation
 @app.head('/', status_code=200)
 def run_head():
     return
